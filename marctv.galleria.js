e583078c43c2bc3fb48cd1d3040c33d991e01d8e
MarcDK <MarcDK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-08 10:02:05 +0000

    Tagging 1.8.2
    git-svn-id: https://plugins.svn.wordpress.org/marctv-galleria/tags/1.8.2@801023 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- marctv.galleria.js
+++ marctv.galleria.js
@@ -1,36 +1,34 @@
 (function($) {
-    
+
   $(document).ready(function($) {
-    if($(".marctv-gallery").length > 0){
-      
+    if ($(".marctv-gallery").length > 0) {
+
       $('body').addClass('no-fullscreen');
-      
+
       Galleria.loadTheme('/wp-content/plugins/marctv-galleria/galleria/themes/marctv/galleria.marctv.js');
-      
+
       $(".marctv-gallery").galleria({
-        width:"auto",
+        width: "auto",
         height: 0.43,
         idleMode: false,
         responsive: true,
         idleTime: 2000,
-        transitionSpeed: 200,
+        transitionSpeed: 300,
         initialTransition: "none",
         debug: false,
         showInfo: true,
         imageCrop: true,
-        imagePanSmoothness: 30,
         fullscreenCrop: true,
         trueFullscreen: true,
         fullscreenDoubleTap: false,
-        easing: 'galleriaOut',
-        thumbnails: true,
+        thumbnails: false,
         dataConfig: function(img) {
           return {
             title: $(img).attr('title'),
             description: $(img).parents('.gallery-item').find('.gallery-caption').text()
           };
-          
-       }
+
+        }
       });
     }
   });
