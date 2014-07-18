3fbd51bfb974fd72645b7ef0eedbbb26fc0876d8
MarcDK <MarcDK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-18 21:31:56 +0000

    Tagging 2.0
    git-svn-id: https://plugins.svn.wordpress.org/marctv-galleria/tags/2.0@951247 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- marctv.galleria-init.js
+++ marctv.galleria-init.js
@@ -1,19 +1,26 @@
 (function($) {
 
   $(document).ready(function($) {
+
+    var thumbnails = true;
+    var height = 0.5;
+
+    if ($(window).width() < 800) {
+      thumbnails = false;
+      height = 0.50;
+      $("body").addClass('no-thumbnails');
+    }
+
     if ($(".marctv-gallery").length > 0) {
 
-      $('body').addClass('no-fullscreen');
-
-      Galleria.loadTheme('/wp-content/plugins/marctv-galleria/galleria/themes/marctv/galleria.marctv.js');
+      Galleria.loadTheme('/wp-content/plugins/marctv-galleria/galleria/themes/classic/galleria.classic.js');
 
       $(".marctv-gallery").galleria({
         width: "auto",
-        height: 0.43,
+        height: height,
         idleMode: false,
         responsive: true,
         idleTime: 2000,
-        transitionSpeed: 300,
         initialTransition: "none",
         debug: false,
         showInfo: true,
@@ -21,15 +28,34 @@
         fullscreenCrop: true,
         trueFullscreen: true,
         fullscreenDoubleTap: false,
-        thumbnails: false,
+        thumbnails: thumbnails,
         dataConfig: function(img) {
           return {
             title: $(img).attr('title'),
             description: $(img).parents('.gallery-item').find('.gallery-caption').text()
           };
 
+        },
+        extend: function() {
+
+          var gallery = this;
+
+          gallery.addElement('info-fullscreen');
+          gallery.append({
+            'info': ['info-fullscreen']
+          });
+
+          gallery.append({
+            'stage': ['info-fullscreen']
+          });
+
+          gallery.$('info-fullscreen').click(function() {
+            gallery.toggleFullscreen(); // toggles the fullscreen
+          });
+         
         }
       });
+
     }
   });
 })(jQuery);
