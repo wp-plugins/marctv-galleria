e309af0ca0f3513814ef6cce4e42ea2bfd42a9df
MarcDK <MarcDK@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-28 13:58:26 +0000

    tagging 2.6.2
    git-svn-id: https://plugins.svn.wordpress.org/marctv-galleria/tags/2.6.2@1188962 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- marctv.galleria-init.js
+++ marctv.galleria-init.js
@@ -1,57 +1,65 @@
-(function($) {
+(function ($) {
 
-  $(document).ready(function($) {
+    $(document).ready(function ($) {
 
-    var thumbnails = true;
-    var height = 0.53;
+        var thumbnails = true;
+        var height = 0.53;
+        var linksize = marctvgalleriajs.linksize;
+        var fullscreen = 'full';
 
-    if ($(window).width() < 800) {
-      thumbnails = false;
-      height = 0.43;
-      $("body").addClass('no-thumbnails');
-    }
 
-    if ($(".marctv-gallery").length > 0) {
+        if ($(window).width() < marctvgalleriajs.breakpoint ) {
+            linksize = marctvgalleriajs.breaksize;
+            fullscreen = marctvgalleriajs.fullscreen;
+            thumbnails = false;
+            height = 0.43;
+            $("body").addClass('no-thumbnails');
+        }
 
-      Galleria.loadTheme('/wp-content/plugins/marctv-galleria/galleria/themes/classic/galleria.classic.js');
+        if ($(".marctv-gallery").length > 0) {
 
-      $(".marctv-gallery").galleria({
-        width: "auto",
-        height: height,
-        idleMode: false,
-        responsive: true,
-        idleTime: 2000,
-        initialTransition: "none",
-        debug: false,
-        showInfo: true,
-        imageCrop: true,
-        fullscreenCrop: true,
-        trueFullscreen: true,
-        fullscreenDoubleTap: false,
-        thumbnails: thumbnails,
-        dataConfig: function(img) {
-          return {
-            title: $(img).attr('title'),
-            description: $(img).parents('.gallery-item').find('.gallery-caption').text()
-          };
+            Galleria.loadTheme('/wp-content/plugins/marctv-galleria/galleria/themes/classic/galleria.classic.js');
 
-        },
-        extend: function() {
+            $(".marctv-gallery").galleria({
+                width: "auto",
+                height: height,
+                idleMode: false,
+                responsive: true,
+                preload: 1,
+                idleTime: 2000,
+                initialTransition: "none",
+                debug: false,
+                showInfo: true,
+                imageCrop: true,
+                fullscreenCrop: true,
+                trueFullscreen: true,
+                fullscreenDoubleTap: false,
+                thumbnails: thumbnails,
+                dataConfig: function (img) {
+                    return {
+                        title: $(img).attr('title'),
+                        description: $(img).parents('.gallery-item').find('.gallery-caption').text(),
+                        image: $(img).data(linksize),
+                        big: $(img).data(fullscreen)
+                    };
 
-          var gallery = this;
+                },
+                extend: function () {
 
-          gallery.addElement('info-fullscreen');
-          gallery.append({
-            'container': ['info-fullscreen']
-          });
+                    var gallery = this;
 
-          gallery.$('info-fullscreen').click(function() {
-            gallery.toggleFullscreen(); // toggles the fullscreen
-          });
+                    gallery.addElement('info-fullscreen');
+                    gallery.append({
+                        'container': ['info-fullscreen']
+                    });
+
+                    gallery.$('info-fullscreen').click(function () {
+                        gallery.toggleFullscreen(); // toggles the fullscreen
+                    });
+
+                }
+            });
 
         }
-      });
-
-    }
-  });
+    });
 })(jQuery);
